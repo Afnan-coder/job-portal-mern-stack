86731910af3f5f6c234a4f6c080e7d56c29ea5e7
Afnan-coder <muhammadafnanali2@gmail.com>
2026-01-23 21:16:51 +0500

    Built the button to show or not the filters on the mobile screen

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,11 +1,12 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { AppContext } from '../context/AppContext'
 import { assets, JobCategories, JobLocations } from '../assets/assets'
 import JobCard from './JobCard'
 
 const JobListing = () => {
 
-    const { isSearched, searchFilter, SetSearchFilter,jobs } = useContext(AppContext)
+    const { isSearched, searchFilter, SetSearchFilter, jobs } = useContext(AppContext)
+    const [showFilter, setShowFilter] = useState(false)
 
     return (
         <div className='container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
@@ -38,8 +39,12 @@ const JobListing = () => {
                     )
                 }
 
+                <button onClick={(e) => setShowFilter(prev => !prev)} className='px-6 py-1.5 border border-gray-400 lg:hidden'>
+                    {showFilter ? "Close" : "Filters"}
+                </button>
+
                 {/* location Filter */}
-                <div className='max-lg:hidden '>
+                <div className={showFilter ? "" : "max-lg:hidden"}>
                     <h4 className='font-medium text-lg py-4'>Search by Catagories</h4>
                     <ul className='space-y-4 text-gray-600'>
                         {
@@ -54,7 +59,7 @@ const JobListing = () => {
                 </div>
 
                 {/* Location Filter */}
-                <div className='max-lg:hidden'>
+                <div className={showFilter ? "" : "max-lg:hidden"}>
                     <h4 className='font-medium text-lg py-4 pt-14'>Search by Location</h4>
                     <ul className='space-y-4 text-gray-600'>
                         {
