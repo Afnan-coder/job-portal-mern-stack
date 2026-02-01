cd06479e18c1f9e55d305a0bb2f2409828805b4f
Afnan-coder <muhammadafnanali2@gmail.com>
2026-02-01 21:27:17 +0500

    Built the Loading component

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams, useSearchParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import Loading from '../components/Loading'
 
 const ApplyJob = () => {
-  return (
-    <div>
-      ApplyJob
-    </div>
+
+  const { id } = useParams()
+  const { jobs } = useContext(AppContext)
+  const [JobData, setJobData] = useState(null)
+
+  const fetchJobs = async () => {
+    const data = await jobs.filter(job => job._id === id)
+    setJobData(data[0])
+    console.log(data[0])
+  }
+
+  useEffect(() => {
+    if (jobs.length > 0) {
+      fetchJobs()
+    }
+  }, [id, jobs])
+
+  return JobData ? (
+    <>
+    
+    </>
   )
+    : (
+     <Loading/>
+    )
 }
 
 export default ApplyJob
