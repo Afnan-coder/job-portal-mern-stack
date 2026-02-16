44033f7b2952a11c421edcf2a64da6be4d6ded99
Afnan-coder <muhammadafnanali2@gmail.com>
2026-02-16 15:56:41 +0500

    Added the Santry(debugging platform) to backend

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,10 @@
 import mongoose from 'mongoose'
 
-// func
+// function to connect with database
+
+const connectDB = async () => {
+    mongoose.connection.on('connected', () => console.log("Database connected"))
+    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
+}
+
+export default connectDB
--- server/server.js
+++ server/server.js
@@ -1,21 +1,37 @@
+import './config/instrument.js'
+import * as Sentry from "@sentry/node"
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/db.js'
 
 // Initilize the express
 const app = express()
 
+// connect to database
+await connectDB()
+
 // Middlewares
 app.use(cors())
 app.use(express.json())
+
+
 
 // Routes
 app.get('/', (req, res) => {
   res.send('Api working')
 })
 
+// Test route
+app.get("/debug-sentry", (req, res) => {
+  throw new Error("My first Sentry error!");
+});
+
+
 const PORT = process.env.PORT || 5000
 
+// Sentry setup
+Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
