51a4a89051196930e1eb83ac6adc156c5391ea3f
Afnan-coder <muhammadafnanali2@gmail.com>
2026-02-20 11:12:27 +0500

    Buit the basic structrue of all company controller functions

---
+++ server/controllers/companyController.js
@@ -0,0 +1,39 @@
+// Register a new company
+export const registerCompany = async (req, res) => {
+
+}
+
+// Company login
+export const loginCompany = async (req, res) => {
+
+}
+
+// Get company data
+export const getCompanyData = async (req, res) => {
+
+}
+
+// Post a new Job
+export const postJob = async (req, res) => {
+
+}
+
+// Get company job applicants
+export const getCompanyJobApplicants = async (req, res) => {
+
+}
+
+// Get company posted jobs
+export const getCompanyPostedJobs = async (req, res) => {
+
+}
+
+// Change job applications status
+export const changeJobApplicationsStatus = async (req, res) => {
+
+}
+
+// Change job visibility
+export const changeVisibility = async (req, res) => {
+
+}
