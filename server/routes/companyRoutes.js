fce32b8064fe78dd4d6f165e8b20c24ccbc3458d
Afnan-coder <muhammadafnanali2@gmail.com>
2026-02-20 11:33:51 +0500

    Built the routes structures for company

---
+++ server/routes/companyRoutes.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import { changeJobApplicationsStatus, changeVisibility, getCompanyData, getCompanyJobApplicants, getCompanyPostedJobs, loginCompany, postJob, registerCompany } from '../controllers/companyController.js'
+
+const router = express.Router()
+
+// Register a company
+router.post('/register', registerCompany)
+
+
+// Company login
+router.post('/login', loginCompany)
+
+
+// Get company data
+router.get('/company', getCompanyData)
+
+
+// Post a job
+router.post('/post-job', postJob)
+
+
+// Get applicants data of company
+router.get('/applicants', getCompanyJobApplicants)
+
+
+// Get company job list
+router.get('/list-job', getCompanyPostedJobs)
+
+
+// Change application status
+router.post('/change-status', changeJobApplicationsStatus)
+
+
+// Change applicatons visibility
+router.post('/change-visibility', changeVisibility)
+
+
+export default router
